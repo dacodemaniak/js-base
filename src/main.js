4ee35338013de4b36b578a2a56e2840db89cf344
DaCodeManiaK <jla.webprojet@gmail.com>
2021-12-03 11:29:14 +0100

    Add timeout function to hide splash

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 
 
 document.addEventListener('DOMContentLoaded', (event) => {
-
+    setTimeout(() => {
+        document.querySelector('div.outer-splash').classList.add('hidden')
+    }, 1000)
 });
