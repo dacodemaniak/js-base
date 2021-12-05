8d731d02c2de0374df8d9808093170203d5a35d1
DaCodeManiaK <jla.webprojet@gmail.com>
2021-12-05 08:55:19 +0100

    Implements home Controller

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -1,6 +1,10 @@
+import { ContactService } from "../services/contact-service";
+import { RowHelper } from "./../_helpers/row-helper";
 export class HomeController {
     
-    constructor() {}
+    constructor() {
+        this.service = new ContactService()
+    }
 
     load(dock) {
         this._dock = dock
@@ -12,7 +16,29 @@ export class HomeController {
     }
 
     _render(template) {
-        this._dock.innerHTML = template;
+        this._dock.innerHTML = template
+
+        const contacts = this.service.findAll()
+
+        for (let contact of contacts) {
+            const row = new RowHelper()
+            row
+                .addColumn('&nbsp;')
+                .addColumn(contact.getLastName())
+                .addColumn(contact.getFirstName())
+                .addColumn(contact.getOccupation())
+                .addColumn(contact.getCompany())
+                .addColumn('&nbsp;')
+    
+                
+            // 4th : Add the whole tr to tbody of our table
+            this._dock.querySelector('tbody').appendChild(row.buildRow())
+        }
+
+        // Get rows number
+        const nbRows = this._dock.querySelectorAll('tbody tr').length
+        console.log(`Got ${nbRows} rows`)
+        this._dock.querySelector('#items-number').innerText = nbRows
     }
 
     _getView() {
--- src/main.js
+++ src/main.js
@@ -25,32 +25,6 @@ window.addEventListener(
 )
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    /*
-    const service = new ContactService()
-
-    const contacts = service.findAll()
-    
-    for (let contact of contacts) {
-        const row = new RowHelper()
-        row
-            .addColumn('&nbsp;')
-            .addColumn(contact.getLastName())
-            .addColumn(contact.getFirstName())
-            .addColumn(contact.getOccupation())
-            .addColumn(contact.getCompany())
-            .addColumn('&nbsp;')
-
-            
-        // 4th : Add the whole tr to tbody of our table
-        document.querySelector('tbody').appendChild(row.buildRow())
-    }
-
-
-
-    // Get rows number
-    const nbRows = document.querySelectorAll('tbody tr').length
-    document.getElementById('items-number').innerText = nbRows
-    */
     setTimeout(() => {
         document.querySelector('div.outer-splash').classList.add('hidden')
     }, 1000)
